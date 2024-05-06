8be295cbc7d4279622c31c4f8bf8016c3c875482
kphill55 <99160057+kphill55@users.noreply.github.com>
2024-05-05 20:37:29 -0400

    Update make_project.bat

--- make_project.bat
+++ make_project.bat
@@ -4,5 +4,5 @@ call vivado -nolog -nojournal -mode=batch -source=./tcl/impl.tcl
 :: commenting out the vitis part since we will be testing in linux
 ::call xsct ./tcl/make_sw.tcl
 ::next line makes a .bit.bin file from the existing .bit file, for use with fpgautil
-call bootgen -image doug.bif -arch zynq -process_bitstream bin -w on
+call bootgen -image kwp.bif -arch zynq -process_bitstream bin -w on
 
